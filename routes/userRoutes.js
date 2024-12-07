b35649377c2652006f5582ad3000c86023c719c0
Krukov Anton <meowskyj@yandex.ru>
2024-12-07 12:18:12 +0300

    add routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,12 @@
 // routes/userRoutes.js
 const express = require('express');
-const { User } = require('../models'); // Импортируйте вашу модель User
+const { User } = require('./../models'); // Импортируйте вашу модель User
 
 const router = express.Router();
 
 // Маршрут для получения данных пользователя по chatId
 router.get('/:chatId', async (req, res) => {
     const { chatId } = req.params;
-
     try {
         const user = await User.findOne({ where: { chatId } });
 
@@ -19,6 +18,7 @@ router.get('/:chatId', async (req, res) => {
     } catch (error) {
         console.error('Ошибка при получении данных пользователя:', error);
         res.status(500).json({ error: 'Ошибка при получении данных пользователя' });
+        
     }
 });
 
