ba751a4502f1e06262518aaf3335abf5aec9c60b
Krukov Anton <meowskyj@yandex.ru>
2024-12-07 12:40:21 +0300

    fix routes

--- routes/updateGarden.js
+++ routes/updateGarden.js
@@ -11,9 +11,11 @@ router.put('/:chatId', async (req, res) => {
         if (!user) {
             return res.status(404).json({ message: 'Пользователь не найден' });
         }
+        console.log('Give data', dataGarden);
+        
         user.userData = dataGarden;
         await user.save();
-        res.status(200).json({ dataGarden: user.userData });
+        res.status(200).json({ userData: user.userData });
     } catch (error) {
         console.error('Ошибка при получении данных пользователя:', error);
         res.status(500).json({ error: 'Ошибка при получении данных пользователя' });
