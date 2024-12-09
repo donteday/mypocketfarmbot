77da3ee7d4b7edfc3f36f57bd25a4b2514fdf874
Krukov Anton <meowskyj@yandex.ru>
2024-12-09 23:06:06 +0300

    fix search

--- routes/findUser.js
+++ routes/findUser.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./models/User'); // Импортируйте вашу модель пользователя
+const User = require('./../models');
 
 router.get('/users', async (req, res) => {
         const { search } = req.query; // Получаем параметр поиска из запроса
